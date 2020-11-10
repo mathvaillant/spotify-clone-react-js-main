8073043025d85fcd77ba0f2236255b3b1e04805e
mathvaillant <matheusvaillant505@gmail.com>
2020-11-10 21:49:10 +0000

    test

---
+++ build/precache-manifest.c71f6b113e8b5207ec333f8c51d830b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb284c2abbc907c9c4117a309e1e2fdc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7039542401fb7ec28c4f",
+    "url": "/static/css/main.148f612c.chunk.css"
+  },
+  {
+    "revision": "e5f96fe9751e6444c069",
+    "url": "/static/js/2.86b7d802.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.86b7d802.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7039542401fb7ec28c4f",
+    "url": "/static/js/main.c2be738d.chunk.js"
+  },
+  {
+    "revision": "eae4d923aa7991f30b4b",
+    "url": "/static/js/runtime-main.3f8caba6.js"
+  }
+]);
